4b426992cf3fd7f97f433752e6dcf058d507ca98
Nazih Najmudin <163629526+nazihstei@users.noreply.github.com>
2025-02-19 12:06:41 +0700

    feat: default seolver

--- src/solver/Solve.java
+++ src/solver/Solve.java
@@ -1,14 +1,48 @@
 package solver;
 
 import model.*;
+
 import java.util.List;
 
 public class Solve {
+    public int tryCount;
+
+    // Constructor
+    public Solve() {
+        this.tryCount = 0;
+    }
     
     // DEFAULT solver
-    public static int bruteForce(Board board, List<List<Block>> blockList) {
-        int forceCount = 0;
+    public boolean checkOneBlock(Board board, List<List<Block>> blockList, int idx, int stopIdx) {
+        boolean isSuccess = false;
+        List<Block> blok = blockList.get(idx);
         
-        return forceCount;
+        for (int i=0; i<board.row; i++) {
+            for (int j=0; j<board.col; j++) {
+                
+                for (Block shape : blok) {
+                    if (idx == stopIdx) {
+                        this.tryCount++;
+                    }
+
+                    boolean isFit = board.putBlock(shape, i, j);
+                    if (isFit) {
+                        if (idx == stopIdx) {
+                            return true;
+                        } else {
+                            isSuccess = checkOneBlock(board, blockList, idx+1, stopIdx);   
+                        }
+                    }
+
+                    if (isSuccess) {
+                        return isSuccess;
+                    } else {
+                        ; // cabut block from board and next shape
+                    }
+                }
+            }
+        }
+        return isSuccess;
     }
+
 }
